8034b50faa4d4befc0e58e8c45f9cc32ee65a0b6
Viet Nguyen <nguyenviet.060695@gmail.com>
2023-09-23 16:31:37 +0700

    fix router

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import authApi from '@/services/auth';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import React, { FormEvent, useState } from 'react';
 
 const Sigup = () => {

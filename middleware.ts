a1e4f2b8ff1a8f21aa1f214a086344e14a1bf861
Viet Nguyen <nguyenviet.060695@gmail.com>
2023-09-23 16:53:32 +0700

    redirect if had cookies

--- middleware.ts
+++ middleware.ts
@@ -4,9 +4,14 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function middleware(req: NextRequest) {
     const token = req.cookies.get('token');
     if (!token) return NextResponse.redirect(new URL('/auth/login', req.url));
+
+    //Redirect If Logined
+    const redirectIfHadCookies = ['/auth/login', '/auth/signup'];
+    if (redirectIfHadCookies.includes(req.nextUrl.pathname))
+        return NextResponse.redirect(new URL('/todo', req.url));
 }
 
 // See "Matching Paths" below to learn more
 export const config = {
-    matcher: ['/todo/:path*'],
+    matcher: ['/todo/:path*', '/auth/:path*'],
 };
